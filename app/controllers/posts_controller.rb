f1b7b5263f81aceed2f521ff4a8b2a5e5baffa0c
rabu1224 <sowahabit5206@gmail.com>
2020-08-02 23:58:26 +0900

    フラッシュメッセージをヘッダー下部に表示できるように設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,46 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
+
+
   def index
+    @posts = Post.order(id: :asc)
   end
 
   def show
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to post, notice: "投稿しました"
   end
 
   def edit
   end
 
   def update
+    @post.update!(post_params)
+    redirect_to @post
+    redirect_to @post, notice: "更新しました"
   end
 
   def destroy
+    @post.destroy!
+    redirect_to @post
+    redirect_to @post, alert: "削除しました"
   end
+
+  private
+
+  def set_post
+  @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
 end
